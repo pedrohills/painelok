39b9090c8594b26677180272a6447e730a99e1d7
Pedro Brito <gpedroemail@gmail.com>
2015-02-16 22:10:23 -0300

    Iniciado desenvolvimento no Controller da tela principal..

--- application/controllers/Painel.php
+++ application/controllers/Painel.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Painel extends OK_Controller {
+class Painel extends CI_Controller {
 
   public function __construct()
   {
